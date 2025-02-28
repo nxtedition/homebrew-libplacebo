41e3b2c023c5feb290e808f1b2ba691c7e242bf4
Niklas Andersson <3985238+niklaspandersson@users.noreply.github.com>
2025-02-28 14:43:40 +0100

    make the formula head-only
    * Needed to force the use of --HEAD when the formula is installed as a dependency (eg from the ffmpeg formula)

--- Formula/libplacebo.rb
+++ Formula/libplacebo.rb
@@ -6,31 +6,6 @@ class Libplacebo < Formula
   license "LGPL-2.1-or-later"
   head "https://github.com/nxtedition/libplacebo.git", branch: "master"
 
-  stable do
-    url "https://code.videolan.org/videolan/libplacebo/-/archive/v7.349.0/libplacebo-v7.349.0.tar.bz2"
-    sha256 "38c9c75d9c1bb412baf34845d1ca58c41a9804d1d0798091d7a8602a0d7c9aa6"
-
-    resource "fast_float" do
-      url "https://github.com/fastfloat/fast_float/archive/refs/tags/v6.1.1.tar.gz"
-      sha256 "10159a4a58ba95fe9389c3c97fe7de9a543622aa0dcc12dd9356d755e9a94cb4"
-    end
-
-    resource "glad2" do
-      url "https://files.pythonhosted.org/packages/15/fc/9235e54b879487f7479f333feddf16ac8c1f198a45ab2e96179b16f17679/glad2-2.0.6.tar.gz"
-      sha256 "08615aed3219ea1c77584bd5961d823bab226f8ac3831d09adf65c6fa877f8ec"
-    end
-
-    resource "jinja2" do
-      url "https://files.pythonhosted.org/packages/ed/55/39036716d19cab0747a5020fc7e907f362fbf48c984b14e62127f7e68e5d/jinja2-3.1.4.tar.gz"
-      sha256 "4a3aee7acbbe7303aede8e9648d13b8bf88a429282aa6122a993f0ac800cb369"
-    end
-
-    resource "markupsafe" do
-      url "https://files.pythonhosted.org/packages/87/5b/aae44c6655f3801e81aa3eef09dbbf012431987ba564d7231722f68df02d/MarkupSafe-2.1.5.tar.gz"
-      sha256 "d283d37a890ba4c1ae73ffadf8046435c76e7bc2247bbb63c00bd1a709c6544b"
-    end
-  end
-
   bottle do
     sha256 cellar: :any, arm64_sequoia:  "3d549716eb833fbc554605706f4e4740f0c1c4e6cf23732326444627ea14a8d5"
     sha256 cellar: :any, arm64_sonoma:   "72afc163cc9dfc5525ed856094449685f034dfbbd8528f04e448b0447dd44f06"
